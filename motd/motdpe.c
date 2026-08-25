82943a48a9db1e2b5d7d222e6b29f8a2dc7dfad9
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2026-08-26 00:05:20 +0800

    修了不能用的问题，也不知道改了什么
    AI太好用了你知道吗

--- motd/motdpe.c
+++ motd/motdpe.c
@@ -1,52 +1,44 @@
-//gcc motdpe.c -o motdpe.exe -lws2_32
-//chcp 65001
+// gcc motdpe.c -o motdpe.exe -lws2_32
+// chcp 65001
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mstcpip.h>
 #include <string.h>
 
 #pragma comment(lib, "ws2_32.lib")
-// play.easecation.net
-#define SERVER_IP "173.208.172.170"
-#define SERVER_PORT 19132
-#define BUF_SIZE 1024
 
-#define DEBUG FALSE 
-// 将十六进制字符串转换为UTF-8字符串
-char* hexToUtf8(const char* hexString,int hexLength) {
-    // printf("hexLength=> %d\n",hexLength);
-    int utf8Length = hexLength / 2;
-    char* utf8String = (char*) malloc(utf8Length + 1);
+#ifndef SIO_UDP_CONNRESET
+#define SIO_UDP_CONNRESET _WSAIOW(IOC_VENDOR, 12)
+#endif
 
-    int i, j;
-    int colorCount = 0;
-    for (i = 0, j = 0; i < hexLength; i += 2, j++) {
-        char hexValue[3] = { hexString[i], hexString[i + 1], '\0' };
-        int decimalValue = strtol(hexValue, NULL, 16);
-        // printf("%c", (char) decimalValue);
-        // if( (char)decimalValue== "§"){
-        //     ++colorCount;
-        // }else{
-            utf8String[j] = (char) decimalValue;
-        // }
-    }
+#define DEFAULT_IP "127.0.0.1"
+#define DEFAULT_PORT 19132
+#define BUF_SIZE 2048
 
-    utf8String[j] = '\0';
-    return utf8String;
-}
- 
-    void hexBufferToString(const unsigned char* buffer, size_t length, char* str_buffer) {
-    for (size_t i = 0; i < length; i++) {
-        sprintf(&str_buffer[i*2], "%02X", buffer[i]);
-    }
-} 
+#define DEBUG FALSE
 
-int main()
+int main(int argc, char *argv[])
 {
+    // 设置 Windows 控制台为 UTF-8 编码，防止中文乱码
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    const char *server_ip = (argc > 1) ? argv[1] : DEFAULT_IP;
+    int server_port = (argc > 2) ? atoi(argv[2]) : DEFAULT_PORT;
+
     WSADATA wsaData;
     SOCKET sock;
     SOCKADDR_IN serverAddr, clientAddr;
-    char sendBuf[] = {0x01, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xc1, 0x1d, 0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78, 0x9c, 0x18, 0x28, 0x7f, 0xe1, 0x64, 0x89, 0x8d};
+
+    // RakNet Unconnected Ping (ID: 0x01, Timestamp: 8B, Magic: 16B, ClientGUID: 8B)
+    char sendBuf[] = {
+        0x01,
+        0x00, 0x00, 0x00, 0x00, 0x21, 0x90, 0xee, 0x82,
+        0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78,
+        0x9c, 0x18, 0x28, 0x7f, 0xe1, 0x64, 0x89, 0x8d
+    };
     char recvBuf[BUF_SIZE];
     int sendLen, recvLen, addrLen;
 
@@ -63,20 +55,22 @@ int main()
         WSACleanup();
         return 1;
     }
-    DWORD dwBytesReturned;
+
+    // 禁用 WSAECONNRESET (Windows UDP 10054 错误忽略)
+    DWORD dwBytesReturned = 0;
     BOOL bNewBehavior = FALSE;
-    DWORD status;
+    WSAIoctl(sock, SIO_UDP_CONNRESET, &bNewBehavior, sizeof(bNewBehavior), NULL, 0, &dwBytesReturned, NULL, NULL);
 
-    status = WSAIoctl(sock, SIO_UDP_CONNRESET, &bNewBehavior, sizeof(bNewBehavior), NULL, 0, &dwBytesReturned, NULL, NULL);
+    // 设置接收超时时间为 3000 ms (Windows Winsock 使用 DWORD 毫秒)
+    DWORD timeout = 3000;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
 
-    if (status == SOCKET_ERROR)
-    {
-        // 处理设置工作模式失败的情况
-    }
     // 配置服务器地址信息
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(SERVER_PORT);
-    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    serverAddr.sin_port = htons((unsigned short)server_port);
+    serverAddr.sin_addr.s_addr = inet_addr(server_ip);
+
+    printf("Connecting to %s:%d ...\n", server_ip, server_port);
 
     // 发送UDP数据包
     sendLen = sendto(sock, sendBuf, sizeof(sendBuf), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
@@ -86,71 +80,66 @@ int main()
         WSACleanup();
         return 1;
     }
-    if(DEBUG)
-    printf("UDP packet sent successfully!\n");
-
-    // 设置超时时间为5秒
-    struct timeval timeout;
-    timeout.tv_sec = 5000;
-    timeout.tv_usec = 0;
-    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
-    if(DEBUG)
-    printf("UDP packet timeout set %d ms!\n",timeout.tv_sec);
 
     // 等待UDP回复
     addrLen = sizeof(clientAddr);
-    recvLen = recvfrom(sock, recvBuf, BUF_SIZE, 0, (SOCKADDR*)&clientAddr, &addrLen);
+    recvLen = recvfrom(sock, recvBuf, BUF_SIZE - 1, 0, (SOCKADDR*)&clientAddr, &addrLen);
     if (recvLen == SOCKET_ERROR) {
-        printf("recvfrom failed with error: %ld\n", WSAGetLastError());
+        printf("recvfrom failed with error: %ld (Timeout or Server Offline)\n", WSAGetLastError());
         closesocket(sock);
         WSACleanup();
         return 1;
     }
 
+    // RakNet Unconnected Pong (ID 0x1c):
+    // 1B ID + 8B PingTime + 8B ServerGUID + 16B Magic + 2B StringLength = 35 Bytes
+    if (recvLen <= 35 || (unsigned char)recvBuf[0] != 0x1c) {
+        printf("Invalid packet received or packet too short (length: %d)\n", recvLen);
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
 
-    // 输出接收到的UDP回复
-    if(DEBUG)
-    printf("UDP packet received from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-    recvLen -= 35;
-    memmove(recvBuf,recvBuf+35,recvLen);
- 
+    int motdLen = recvLen - 35;
+    char *motd = recvBuf + 35;
+    motd[motdLen] = '\0'; // 确保字符串以 \0 结尾
 
-    char str_buffer[recvLen * 2 + 1]; // 预留一个额外的字符用于存储字符串结束符'\0'
-    hexBufferToString(recvBuf, recvLen, str_buffer);
-    // printf("\nend\n");
-    // printf("\n");
+    printf("\n=== MOTD 原始数据 ===\n%s\n\n=== 解析结果 ===\n", motd);
 
-    char* motd = hexToUtf8(str_buffer,recvLen * 2 + 1);
-    // char* motd
-    // printf("转换后的字符串: %s\n", motd);
-    char *token;
-    
-    char *sign[] = {"服务器类型: ", "服务器名称: ", "协议=>", "版本=>", "在线人数: ", "最大人数: ","","地图名=> ", "默认模式: ", "默认模式: ", "IPV4端口: ",  "IPV6端口: ", "0=" };
-    char pos = 0; 
+    const char *sign[] = {
+        "服务器类型: ",
+        "服务器名称: ",
+        "协议版本: ",
+        "游戏版本: ",
+        "在线人数: ",
+        "最大人数: ",
+        "Server GUID: ",
+        "副标题/地图: ",
+        "游戏模式: ",
+        "模式数值: ",
+        "IPv4 端口: ",
+        "IPv6 端口: ",
+        "额外参数1: ",
+        "额外参数2: ",
+        "额外参数3: ",
+        "额外参数4: "
+    };
+    int maxSigns = sizeof(sign) / sizeof(sign[0]);
+    int pos = 0;
 
-    // 使用 strtok 函数分割字符串
-    token = strtok(motd, ";");
-
-    // 循环打印分割后的子字符串
+    // 使用 strtok 分割字符串
+    char *token = strtok(motd, ";");
     while (token != NULL) {
-        printf("%s%s\n",sign[pos++], token);
-        // printf("%s\n",  token);
+        if (pos < maxSigns) {
+            printf("%-14s %s\n", sign[pos], token);
+        } else {
+            printf("扩展字段[%02d]:  %s\n", pos, token);
+        }
+        pos++;
         token = strtok(NULL, ";");
     }
-    // printf("\n接收到的字符串");
-    // printf("\n %c",newchar);
 
-
-
-    // printf("\n");
-    // printf("\n");
-    // printf("\n");
-
-    // 关闭UDP套接字
     closesocket(sock);
-    // free(newchar);
-    // 卸载Winsock库
     WSACleanup();
-
     return 0;
 }
